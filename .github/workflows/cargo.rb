f99c2a736a3717931ff891d248e68f100fa884ab
Web4 <167559384+Web4application@users.noreply.github.com>
2024-10-26 10:25:01 -0400

    Update and rename cargo.rb to .github/workflows/cargo.rb

--- .github/workflows/cargo.rb
+++ .github/workflows/cargo.rb
@@ -13,7 +13,7 @@ require "dependabot/cargo/version"
 
 require "dependabot/pull_request_creator/labeler"
 Dependabot::PullRequestCreator::Labeler
-  .register_label_details("cargo", name: "roda", colour: "000000")
+  .register_label_details("cargo", name: "roda", colour: "3e0018")
 
 require "dependabot/dependency"
 Dependabot::Dependency.register_production_check("cargo", ->(_) { true })
