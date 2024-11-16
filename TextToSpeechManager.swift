31b92501d5806c23e6ca9755e57c4c64515717cf
Web4 <167559384+Web4application@users.noreply.github.com>
2024-11-16 11:22:28 -0500

    Update TextToSpeechManager.swift

--- TextToSpeechManager.swift
+++ TextToSpeechManager.swift
@@ -2,12 +2,12 @@ import AVFoundation
 
 class TextToSpeechManager: ObservableObject {
     private let synthesizer = AVSpeechSynthesizer()
-    
+
     func speak(text: String) {
         let utterance = AVSpeechUtterance(string: text)
         utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
         utterance.rate = 0.5
-        
+
         synthesizer.speak(utterance)
     }
 }
