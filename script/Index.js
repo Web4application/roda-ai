d3a7b69170902165a06dce740b4d7abeeb4255da
Web4 <167559384+Web4application@users.noreply.github.com>
2025-01-28 02:54:57 +0100

    Update Index.js

--- script/Index.js
+++ script/Index.js
@@ -1,33 +1,35 @@
-const ORIGIN_URL = 'https://roda.sandraisom.workers.dev';
-const EXPERIMENTS = [
-  { name: 'big-button', threshold: 0.5 }, // enable the Big Button experiment for 50% of users
-  { name: 'new-brand', threshold: 0.1 }, // enable the New Brand experiment for 10% of users
-  { name: 'new-layout', threshold: 0.02 }, // enable the New Layout experiment for 2% of users
-];
+const ORIGIN_URL = process.env.ORIGIN_URL || 'https://roda.sandraisom.workers.dev';'http://localhost:8000';'https://nextjs-with-supabase-4a7oim3f6-web4apps.vercel.app';
+const BIG_BUTTON_THRESHOLD = 0.5;
+const NEW_BRAND_THRESHOLD = 0.1;
+const NEW_LAYOUT_THRESHOLD = 0.02;
 
+const EXPERIMENTS = [
+  { name: 'big-button', threshold: BIG_BUTTON_THRESHOLD }, // enable the Big Button experiment for 50% of users
+  { name: 'new-brand', threshold: NEW_BRAND_THRESHOLD }, // enable the New Brand experiment for 10% of users
+  { name: 'new-layout', threshold: NEW_LAYOUT_THRESHOLD }, // enable the New Layout experiment for 2% of users
+];
 
 export default {
   async fetch(request, env, ctx) {
-    const fingerprint = [request.headers.get('cf-connecting-ip'), request.cf?.postalCode]; // add any values you want considered as a fingerprint
+    const ip = request.headers.get('cf-connecting-ip') || '';
+    const postalCode = request.cf?.postalCode || '';
+    const fingerprint = [ip, postalCode];
     const activeExperiments = await getActiveExperiments(fingerprint, EXPERIMENTS);
 
-
-    // add a data-experiments attribute to the <body> tag
-    // which can be styled in CSS with a wildcard selector like [data-experiments*="big-button"]
     const rewriter = new HTMLRewriter().on('body', {
       element(element) {
         element.setAttribute('data-experiments', activeExperiments.join(' '));
       },
     });
 
-
-    const res = await fetch(ORIGIN_URL, request);
-
-
-    return rewriter.transform(res);
+    try {
+      const res = await fetch(ORIGIN_URL, request);
+      return rewriter.transform(res);
+    } catch (error) {
+      return new Response('Error fetching the URL', { status: 500 });
+    }
   },
 };
-
 
 // Get active experiments by hashing a fingerprint
 async function getActiveExperiments(fingerprint, experiments) {
@@ -37,7 +39,6 @@ async function getActiveExperiments(fingerprint, experiments) {
   return activeExperiments.map((exp) => exp.name);
 }
 
-
 // Hash a string using the Web Crypto API
 async function hash(algorithm, message) {
   const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
@@ -45,4 +46,3 @@ async function hash(algorithm, message) {
   const hashArray = new Uint8Array(hashBuffer); // convert buffer to byte array
   return hashArray;
 }
-
