36c3b35510b909dcf7d181ac7645c8a269447a61
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-03 10:32:34 -0400

    Update and rename App.tsx to App/room.tsx

--- App/room.tsx
+++ App/room.tsx
@@ -3,14 +3,17 @@
 import {
   LiveblocksProvider,
   RoomProvider,
+  ClientSideSuspense,
 } from "@liveblocks/react/suspense";
-import { Editor } from "./Editor";
+import { CollaborativeEditor } from "./CollaborativeEditor";
 
 export default function App() {
   return (
     <LiveblocksProvider publicApiKey={"pk_prod_9KensGJpifraqw4oar3bVPl2T5Qh5W4NWUpdY0MwaUjA3RMH8gCLjfZyky9lyF77"}>
       <RoomProvider id="my-room">
-        {/* ... */}
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          <CollaborativeEditor />
+        </ClientSideSuspense>
       </RoomProvider>
     </LiveblocksProvider>
   );
