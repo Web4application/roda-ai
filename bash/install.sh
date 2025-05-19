666af7a4dd69d9717aac98437397a2e6ae33ebdd
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-18 18:07:55 -0700

    Update install.sh

--- bash/install.sh
+++ bash/install.sh
@@ -1,4 +1,5 @@
 npm install @google-cloud/translate @aws-sdk/client-s3 @aws-sdk/client-comprehend dialogflow firebase @auth0/auth0-spa-js @google-cloud/speech @google-cloud/text-to-speech @google/analytics-data @newrelic/agent
 npx serve ./UI
 # or
-python3 -m http.server 8000
+python3 -m http.server 5000
+pip install -r requirements.txt
