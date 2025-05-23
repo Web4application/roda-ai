9d6a1eb29e9f5c8177495e40cb40b19d02be0ad0
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-23 14:48:08 -0700

    Update Homebrew.sh

--- Homebrew.sh
+++ Homebrew.sh
@@ -1,4 +1,5 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
 pip install python-dotenv psycopg2
 git add .
