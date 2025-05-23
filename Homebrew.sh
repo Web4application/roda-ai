c662be5e2d0445f623b9f588a23d5252aab3f0cc
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-23 14:47:39 -0700

    Update Homebrew.sh

--- Homebrew.sh
+++ Homebrew.sh
@@ -1,3 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+npm install @liveblocks/client @liveblocks/react @liveblocks/yjs yjs @monaco-editor/react y-monaco y-protocols
+pip install python-dotenv psycopg2
 git add .
 git commit -m "Added files from phone"
 git push origin main
