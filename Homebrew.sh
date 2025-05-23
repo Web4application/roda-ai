49cd2cac1fb684e0b13f98871096326b371a5e7b
Web4 <167559384+Web4application@users.noreply.github.com>
2025-05-23 14:46:36 -0700

    Update Homebrew.sh

--- Homebrew.sh
+++ Homebrew.sh
@@ -1,2 +1,6 @@
+git add .
+git commit -m "Added files from phone"
+git push origin main
+pip install -r requirements.txt
 npx create-liveblocks-app@latest --init --framework react
 git clone https://github.com/QUBUHUB-repos/RODA-AI-Web.git
