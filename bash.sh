d1926369946253605be7851a16305f258657cb7c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-08 17:02:42 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,3 +2,8 @@ git clone https://github.com/Web4application/RODAAI.git
 cd RODAAI
 cp .env.example .env
 docker-compose up --build
+
+git clone https://github.com/Web4application/enclov-AI.git
+cd enclov-AI
+
+docker-compose up --build
