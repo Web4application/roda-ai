3e11a6a010aa45b18a6283fffc6dd24572fe0e4f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-08 18:39:03 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -7,3 +7,9 @@ git clone https://github.com/Web4application/enclov-AI.git
 cd enclov-AI
 
 docker-compose up --build
+
+npm i -g vercel
+vercel login
+vercel link
+vercel build        # creates `.vercel/output`
+vercel deploy --prebuilt  # deploys from that build
