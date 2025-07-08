7db5c081e5891b55ecf25c24e683bdde9f261482
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-08 18:48:09 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -13,3 +13,6 @@ vercel login
 vercel link
 vercel build        # creates `.vercel/output`
 vercel deploy --prebuilt  # deploys from that build
+
+vercel env add team_bnSuCzLCbrlG4vo5dvRRaj0D
+vercel env add c776CzCNUv1dDy9PADHdVmZT
