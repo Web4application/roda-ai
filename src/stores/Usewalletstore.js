e1282752466ca1a36f14b0531d78d34ae31009e2
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-24 20:59:12 +0200

    Update Usewalletstore.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- src/stores/Usewalletstore.js
+++ src/stores/Usewalletstore.js
@@ -1,1 +1,20 @@
+import { defineStore } from 'pinia'
 
+export const useWalletStore = defineStore('wallet', {
+  state: () => ({
+    address: null,
+    balance: null,
+    provider: null,
+  }),
+  actions: {
+    async connectWallet() {
+      if (window.ethereum) {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const accounts = await provider.send('eth_requestAccounts', [])
+        this.address = accounts[0]
+        this.provider = provider
+        this.balance = await provider.getBalance(this.address)
+      }
+    },
+  },
+})
