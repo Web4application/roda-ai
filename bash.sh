55864d9e3b73f3dc8f12dfe7dd11e2190b1c5ef9
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-24 21:56:36 +0200

    Update bash.sh
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 npm install -r requirements.txt
 # Step 1: Build Docker image
-docker build -t roda-api .
+docker build -t roda-ai-api .
 
 # Step 2: Run container
 docker run -d -p 8000:8000 roda-ai-api
